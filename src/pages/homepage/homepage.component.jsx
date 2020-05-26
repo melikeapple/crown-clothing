cac73e46a46d6b835330bee7c79c136c3b2c0763
melike elma <melikapple.80@gmail.com>
2020-05-26 20:28:01 +0300

    adding header component and links to home,shop page and contact

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Directory from "../../components/directory/directory.component";
+import Header from "../../components/header/header-component";
 import './homepage.styles.scss';
 
 const HomePage = () => (
